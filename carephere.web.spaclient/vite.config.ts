d1b23df8da49469b6664e26fc8e7a3fb66fab771
Anuj Kumar Jha <net.anuj@gmail.com>
2025-02-08 15:22:53 +0530

    test config

--- carephere.web.spaclient/vite.config.ts
+++ carephere.web.spaclient/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Load environment variables based on the mode
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd());
 
@@ -16,6 +15,12 @@ export default defineConfig(({ mode }) => {
             __APP_ENV__: JSON.stringify(env.VITE_APP_ENV),
             VITE_APP_URI: JSON.stringify(env.VITE_APP_URI),
         },
+        root: ".", // Ensure Vite starts from the project root
+        build: {
+            outDir: "dist",  // Ensure build output is placed in `dist/`
+            emptyOutDir: true,  // Clears the output directory before building
+        },
     };
 });
+
 
